1c3e5f5be6581703477e7750099c5b5015c06268
Sean Joseph <theseanjoseph@gmail.com>
2020-03-27 16:09:04 -0400

    Added messages for all logic paths in helper functions

--- installation_scripts/installation-functions.sh
+++ installation_scripts/installation-functions.sh
@@ -4,6 +4,7 @@ create-directory-if-not-exists () {
   if [ -d $1 ]; then
     echo "$1 already exists. Skipping..."
   else
+	echo "Creating $1"
     mkdir $1
   fi;
 }
@@ -11,12 +12,14 @@ create-directory-if-not-exists () {
 update-file () {
   if [ -f $2 ]; then
     if diff $2 $1 > /dev/null; then
-      echo "$3 file is the same. Skipping..."
+      echo "$3 is the same. Skipping..."
     else
+	  echo "$3 already exists. Backing up and copying..."	
       mv $2 "$2.bak"
       cp $1 $2
     fi
   else
+	echo "Copying $3"
     cp $1 $2
   fi;
 }
