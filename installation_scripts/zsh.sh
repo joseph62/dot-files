d0d7d613ecb5e522308bf2918b7c59402dc25699
Sean Joseph <theseanjoseph@gmail.com>
2020-03-27 16:40:00 -0400

    Fixed zsh-syntax-highlighting install

--- installation_scripts/zsh.sh
+++ installation_scripts/zsh.sh
@@ -7,14 +7,11 @@ update-file resources/aliases ~/.aliases 'aliases'
 update-file resources/environment ~/.environment 'environment'
 update-file resources/zshrc ~/.zshrc 'zshrc'
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting /tmp/zsh-syntax-highlighting
-
 create-directory-if-not-exists ~/.local
 create-directory-if-not-exists ~/.local/opt
-create-directory-if-not-exists ~/.local/opt/zsh-syntax-highlighting
 
-update-file /tmp/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/.local/opt/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 'zsh-syntax-highlighting'
+rm -r ~/.local/opt/zsh-syntax-highlighting
 
-rm -r /tmp/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.local/opt/zsh-syntax-highlighting
 
 echo 'Zsh Installed'
