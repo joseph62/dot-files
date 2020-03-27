5a60c37c50ee7425c0e85ab4eac9f6f221964433
Sean Joseph <theseanjoseph@gmail.com>
2020-03-27 16:24:40 -0400

    Modified zshrc to use zsh-syntax-highlighting in profile and zsh-syntax-highlighting install logic

--- installation_scripts/zsh.sh
+++ installation_scripts/zsh.sh
@@ -7,6 +7,14 @@ update-file resources/aliases ~/.aliases 'aliases'
 update-file resources/environment ~/.environment 'environment'
 update-file resources/zshrc ~/.zshrc 'zshrc'
 
-# Download zsh-syntax-highlighting if it does not exist
+git clone https://github.com/zsh-users/zsh-syntax-highlighting /tmp/zsh-syntax-highlighting
+
+create-directory-if-not-exists ~/.local
+create-directory-if-not-exists ~/.local/opt
+create-directory-if-not-exists ~/.local/opt/zsh-syntax-highlighting
+
+update-file /tmp/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/.local/opt/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 'zsh-syntax-highlighting'
+
+rm -r /tmp/zsh-syntax-highlighting
 
 echo 'Zsh Installed'
