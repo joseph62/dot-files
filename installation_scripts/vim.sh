a31cc81c377b0d45b835cb46c92fc54a23db09ff
Sean Joseph <theseanjoseph@gmail.com>
2020-04-26 14:42:56 -0400

    Added vundle to vim install script

--- installation_scripts/vim.sh
+++ installation_scripts/vim.sh
@@ -7,4 +7,9 @@ update-file resources/vimrc ~/.vimrc 'vimrc'
 
 create-directory-if-not-exists ~/temp
 
+create-directory-if-not-exists ~/.vim
+create-directory-if-not-exists ~/.vim/bundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo 'Vim Installed'
