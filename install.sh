9df578866fc3ff6b110018a27a46079320903bad
Sean Joseph <theseanjoseph@gmail.com>
2020-05-19 20:49:27 -0400

    Removed install script in favor of the system-setup repository

--- install.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-echo 'Running installation scripts...'
-ls installation_scripts | xargs -I '{}' bash 'installation_scripts/{}'
-echo 'Installation scripts completed!'
--- installation_scripts/git.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-echo 'Installing git...'
-
-. installation_scripts/installation-functions.sh
-
-update-file resources/gitconfig ~/.gitconfig 'gitconfig'
-
-echo 'Git Installed'
